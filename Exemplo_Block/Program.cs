675eb66db09ada9ddddfaa4078c12d3815156cf2
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-08-17 19:23:07 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo_Block/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo_Block
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
